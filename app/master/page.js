58534bf4e5f76956fb93a6dc887b07619f6d0faa
Shashank Bandari <shashank2bandari@gmail.com>
2025-03-21 08:27:04 +0530

    master againnnnnmmm

--- app/master/page.js
+++ app/master/page.js
@@ -24,7 +24,7 @@ export default function MasterHome() {
       const fetchedData = {};
 
       try {
-        for (const user of validUsers) {
+        if (1) {
           const blobUrl = `https://jjat2xf6azudepf3.public.blob.vercel-storage.com/users/${user}.json?t=${timestamp}`;
           const res = await fetch(blobUrl, { cache: 'no-store' });
           if (res.ok) {
